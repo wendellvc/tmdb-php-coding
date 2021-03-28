d467183d1f3c747e545760903f9d2c5042e3c6e1
cabalhinwendell@gmail.com <cabalhinwendell@gmail.com>
2021-03-28 19:06:17 +0800

    updates

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -19,12 +19,9 @@ class MoviesController extends Controller
      */
      public function index()
      {
-        $tmdb = new TMDB();
-        /*
-          Insert API key
-          Generate API Key - https://www.themoviedb.org/settings/api
-        */
-      	$tmdb->setAPIKey('719791a81d8142cc0a14f4516a52a59f');
+        $tmdb = $this->api_settings();
+
+        // get list of popular movies
         $movies = $tmdb->getPopularMovies('sort_by=popularity.desc');
 
         return view('movies.index', compact('movies'));
@@ -38,16 +35,26 @@ class MoviesController extends Controller
      */
     public function show($id)
     {
-        $tmdb = new TMDB();
-        /*
-          Insert API key
-          Generate API Key - https://www.themoviedb.org/settings/api
-        */
-        $tmdb->setAPIKey('719791a81d8142cc0a14f4516a52a59f');
+        $tmdb = $this->api_settings();
+
+        // get movie details by ID
         $movie = $tmdb->getMovie($id);
 
         // Pass current movie details to view
         return view('movies.show', compact('movie'));
+    }
+
+    public function api_settings(){
+
+      // initialize the TMDB class
+      $tmdb = new TMDB();
+      /*
+        Insert API key
+        Generate API Key - https://www.themoviedb.org/settings/api
+      */
+      $tmdb->setAPIKey('719791a81d8142cc0a14f4516a52a59f');
+
+      return $tmdb;
     }
 
 }
